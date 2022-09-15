80cf90b3d151dab865ea8566a1bbd57256f6bd03
p23backerman <p23backerman@fairfieldprep.org>
2022-09-15 10:52:33 -0400

    lab 2-3

---
+++ lab_2-3.py
@@ -0,0 +1,20 @@
+a=2
+
+print(type(a))
+
+a = float(a)
+
+print(type(a))
+
+a = '2'
+
+print(type(a))
+
+#print(a == 2)
+
+a = bool(a)
+print (a)
+
+print( a == 2)
+#print (true == 2)
+#The result is false because the value of a is true
