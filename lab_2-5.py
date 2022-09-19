8d8e9915bdc1c870a4234daf4e42388d52d4640f
p23backerman <p23backerman@fairfieldprep.org>
2022-09-19 12:08:16 -0400

    2-5

---
+++ lab_2-5.py
@@ -0,0 +1,28 @@
+x=21
+y=21
+z=21
+w=21
+t=21
+a=21
+i=21
+#x += 5
+x = x+5
+print(x)
+#y=y-3
+y -= 3
+print(y)
+#z **= 2
+z = z**2
+print(z)
+#w=w % 2
+w%=2
+print(w)
+#t=t/5
+t/=5
+print(t)
+#a*=12
+a = a*12
+print(a)
+#i=i//7
+i//=7
+print(i)
