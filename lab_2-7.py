2171e3f2c35fa429944aed056d1acd53b03e69c8
p23backerman <p23backerman@fairfieldprep.org>
2022-09-20 13:40:27 -0400

    2-6 and 2-7

---
+++ lab_2-7.py
@@ -0,0 +1,39 @@
+from asyncio import events
+
+
+a=5
+b=10
+c=0
+d=-4
+if a < -5:
+        print('a is less than -5')
+elif -5 < a < 5:
+        print('a is between -5 and 5')
+elif a > 5:
+        print('a is greater than 5')
+else:
+    print('a = 5')
+if b < -5:
+        print('b is less than -5')
+elif -5 < b < 5:
+        print('b is between -5 and 5')
+elif b > 5:
+        print('b is greater than 5')
+else:
+    print('b = 5')
+if c < -5:
+        print('c is less than -5')
+elif -5 < c < 5:
+        print('c is between -5 and 5')
+elif c > 5:
+        print('c is greater than 5')
+else:
+    print('c = 5')
+if d < -5:
+        print('d is less than -5')
+elif -5 < d < 5:
+        print('d is between -5 and 5')
+elif d > 5:
+        print('d is greater than 5')
+else:
+    print('d = 5')
